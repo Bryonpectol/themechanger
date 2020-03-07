04709f9b7fa279b65aeca77ceff2771cd62dc481
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2020-03-07 10:04:59 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,80 @@
+let firstDiv = document.getElementById("firstdiv");
+firstDiv.className = "firDiv";
+
+let secondDiv = document.getElementById("seconddiv");
+secondDiv.className = "secDiv";
+
+let thirdDiv = document.getElementById("thirddiv");
+thirdDiv.className = "thiDiv";
+
+let fourthDiv = document.getElementById("fourthdiv");
+fourthDiv.className = "fouDiv";
+
+let fifthDiv = document.getElementById("fifthdiv");
+fifthDiv.className = "fifDiv";
+let sixthDiv = document.getElementById("sixthdiv");
+
+
+function firstFunction() {
+  firstDiv.className = "firDiv";
+  secondDiv.className = "secDiv";
+  thirdDiv.className = "thiDiv";
+  fourthDiv.className = "fouDiv";
+  fifthDiv.className = "fivDiv";
+};
+
+function secondFunction() {
+  firstDiv.className = "firFDiv";
+  secondDiv.className = "firSDiv";
+  thirdDiv.className = "firTDiv";
+  fourthDiv.className = "firFoDiv";
+  fifthDiv.className = "firFiDiv";
+};
+
+function thirdFunction() {
+  firstDiv.className = "secFDiv";
+  secondDiv.className = "secSDiv";
+  thirdDiv.className = "secTDiv";
+  fourthDiv.className = "secFoDiv";
+  fifthDiv.className = "secFiDiv";
+};
+
+function fourthFunction() {
+  firstDiv.className = "thiFDiv";
+  secondDiv.className = "thiSDiv";
+  thirdDiv.className = "thiTDiv";
+  fourthDiv.className = "thiFoDiv";
+  fifthDiv.className = "thiFiDiv";
+};
+
+function fifthFunction() {
+  firstDiv.className = "fouFDiv";
+  secondDiv.className = "fouSDiv";
+  thirdDiv.className = "fouTDiv";
+  fourthDiv.className = "fouFoDiv";
+  fifthDiv.className = "fouFiDiv";
+};
+
+function sixthFunction() {
+  firstDiv.className = "fifFDiv";
+  secondDiv.className = "fifSDiv";
+  thirdDiv.className = "fifTDiv";
+  fourthDiv.className = "fifFoDiv";
+  fifthDiv.className = "fifFiDiv";
+};
+
+function seventhFunction() {
+  firstDiv.className = "sixFDiv";
+  secondDiv.className = "sixSDiv";
+  thirdDiv.className = "sixTDiv";
+  fourthDiv.className = "sixFoDiv";
+  fifthDiv.className = "sixFiDiv";
+};
+
+function eighthFunction() {
+  firstDiv.className = "sevFDiv";
+  secondDiv.className = "sevSDiv";
+  thirdDiv.className = "sevTDiv";
+  fourthDiv.className = "sevFoDiv";
+  fifthDiv.className = "sevFiDiv";
+};
